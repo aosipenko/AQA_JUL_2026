d86584c4eb62ac87ce5f566216424bee5137d8f3
byandy99 <byandy99@gmail.com>
2026-09-22 20:02:06 +0400

    HomeWork10

---
+++ src/org/prog/session10/homework10/PhoneException.java
@@ -0,0 +1,9 @@
+package org.prog.session10.homework10;
+
+public class PhoneException extends RuntimeException{
+    public PhoneException() {
+    }
+    public PhoneException(String message) {
+        super(message);
+    }
+}
